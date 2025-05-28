bde8a59dd4b6cb969159790abca9f97a50f8c132
Zeynep ARIKAN <zynparkn2110@gmail.com>
2025-05-28 22:43:00 +0300

    SQL_Odev2 add

---
+++ SQL_Dersi/SQL_odev2.sql
@@ -0,0 +1,5 @@
+select * from film where replacement_cost BETWEEN 12.99 AND 16.99
+
+Select first_name , last_name from actor Where first_name IN ('Penelope','Nick', 'Ed')
+
+Select * from film where rental_rate IN ( 0.99, 2.99, 4.99) and replacement_cost IN (12.99, 15.99, 28.99)
