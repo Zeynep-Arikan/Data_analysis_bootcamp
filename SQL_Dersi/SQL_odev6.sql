a5e9cdb124a601d64f7fbd8a7650e0f856a6ccc0
Zeynep ARIKAN <zynparkn2110@gmail.com>
2025-06-03 23:00:48 +0300

    sql-odev-6-eklendi

---
+++ SQL_Dersi/SQL_odev6.sql
@@ -0,0 +1,15 @@
+﻿--film tablosunda bulunan rental_rate sütunundaki değerlerin ortalaması nedir?
+--film tablosunda bulunan filmlerden kaç tanesi 'C' karakteri ile başlar?
+--film tablosunda bulunan filmlerden rental_rate değeri 0.99 a eşit olan en uzun (length) film kaç dakikadır?
+--film tablosunda bulunan filmlerin uzunluğu 150 dakikadan büyük olanlarına ait kaç farklı replacement_cost değeri vardır?
+
+Select AVG(rental_rate) from film
+
+SELECT COUNT(*) FROM film
+WHERE title LIKE 'C%';
+
+select MAX(length) from film 
+where rental_rate = 0.99
+
+SELECT COUNT(DISTINCT replacement_cost) FROM film
+WHERE length > 150;
